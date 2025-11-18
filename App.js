3834e9b48dfc9dea8fd7f16adb4b07e1818cac80
pyang-droid <1700876@ub.edu.ph>
2025-11-18 22:56:34 +0800

    Updated App.js

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import Flex from "./Components/flex"; 
+import Flex2 from "./Components/flex_direction";
+import LayoutDirection from "./Components/Layout Direction";
+import JustifyContent from "./Components/Justify Content";
+import { ScrollView, View } from "react-native";
+import AlignItems from "./Components/Align Items";
+import AlignContent from "./Components/Align Content";
+import FlexWrap from "./Components/Flex Wrap";
+import FlexBasis from "./Components/Flex Basis";
+import RowGap from "./Components/Row Gap";
+import WidthHeight from "./Components/WidthHeight";
+import Position from "./Components/Position";
+
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+   <ScrollView>
+   <View style={{height: 600}}>
+    <Flex />
     </View>
+    
+    <View style={{height: 600}}>
+    <Flex2 /> 
+    </View>
+
+    <View style={{height: 600}}>
+    <LayoutDirection />
+    </View>
+
+    <View style={{height: 600}}>
+    <JustifyContent />
+    </View> 
+
+    <View style={{height: 600}}>
+    <AlignItems />
+    </View>
+
+    <View style={{height: 600}}>
+    <AlignContent />
+    </View>
+
+    <View style={{height: 600}}>
+    <FlexWrap />
+    </View>
+
+    <View style={{height: 600}}>
+    <FlexBasis />
+    </View>
+
+    <View style={{height: 600}}>
+    <RowGap />
+    </View>
+
+    <View style={{height: 600}}>
+    <WidthHeight />
+    </View> 
+
+    <View style={{height: 600}}>
+    <Position />
+    </View>
+
+    <StatusBar style="auto" />
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
